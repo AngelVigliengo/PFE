188aaf4e8121a760ee04058cb881fa3fbd405590
PierreLouisBaudrand <43134780+PierreLouisBaudrand@users.noreply.github.com>
2023-08-24 16:56:55 +0200

    +acrophobie

---
+++ Assets/Scripts/SoundCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCollision : MonoBehaviour
+{
+    public AudioSource sourceson;
+    public AudioClip clip1, clip2;
+    
+    void Start()
+    {
+        sourceson = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ZoneSon")
+        {
+            sourceson.clip = clip1;
+            sourceson.Play();
+        }
+        if (collision.gameObject.tag == "ZoneSonPeur")
+        {
+            sourceson.clip = clip2;
+            sourceson.Play();
+        }
+    }
+}
