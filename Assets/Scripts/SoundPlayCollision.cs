5ac8280d26ecec84af9666e477e70decab8cd04e
PierreLouisBaudrand <43134780+PierreLouisBaudrand@users.noreply.github.com>
2023-08-24 14:09:40 +0200

    fix gitignore

---
+++ Assets/Scripts/SoundPlayCollision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayCollision : MonoBehaviour
+{
+    public AudioSource sourceson;
+    public AudioClip clip1, clip2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sourceson = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter (Collision collision)
+    {
+        if (collision.gameObject.tag == "ZoneSon")
+        {
+            sourceson.clip = clip1;
+            sourceson.Play();
+        }
+        if (collision.gameObject.tag == "ZoneSonPeur")
+        {
+            sourceson.clip = clip2;
+            sourceson.Play();
+        }
+    }
+}
