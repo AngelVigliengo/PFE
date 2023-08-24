150128cc40b7cc91a3cbbf8c5d4472676dab6b18
AngelVigliengo <angel.vigliengo@limayrac.fr>
2023-08-24 15:59:49 +0100

    finish aracno level

--- Assets/Scripts/LevelSelectionMenu.cs
+++ Assets/Scripts/LevelSelectionMenu.cs
@@ -20,6 +20,7 @@ public class LevelSelectionMenu : NetworkBehaviour
 
     public Dropdown arachnophobieButton;
     public Dropdown acrophobieButton;
+    public Dropdown ophiophobieButton;
     public GameObject patientPrefab;
 
 
@@ -135,14 +136,10 @@ public class LevelSelectionMenu : NetworkBehaviour
 
     public void AracnophobieLevelSelection()
     {
-
-        //ResetAudioAndSkybox();
-        //SetNormalLevel();
-
         int selectedIndex = arachnophobieButton.value;
         localPlayer.CmdChangeSkybox(selectedIndex);
 
-        switch (arachnophobieButton.value)
+        switch (selectedIndex)
         {
             case 0:
                 TeleportPatientToPosition(new Vector3(25f, 30f, 25f));
@@ -153,20 +150,58 @@ public class LevelSelectionMenu : NetworkBehaviour
                 localPlayer.RpcChangeAudioIndex(1,0);
                 break;
             case 2:
-                TeleportPatientToPosition(new Vector3(5f, 1f, 3f));
-                localPlayer.RpcChangeAudioIndex(0,0);
+                TeleportPatientToPosition(new Vector3(25f, 30f, 25f));
+                localPlayer.RpcChangeAudioIndex(2,0);
                 break;
             case 3:
                 TeleportPatientToPosition(new Vector3(5f, 1f, 3f));
                 localPlayer.RpcChangeAudioIndex(0,0);
                 break;
             case 4:
-                TeleportPatientToPosition(new Vector3(5f, 1f, 3f));
+                TeleportPatientToPosition(new Vector3(24f, 1f, 3f));
                 localPlayer.RpcChangeAudioIndex(0,0);
                 break;
             case 5:
                 TeleportPatientToPosition(new Vector3(13.5f, 09f, 3f));
+                localPlayer.RpcChangeAudioIndex(0,0);
+                break;
+            case 6:
+                TeleportPatientToPosition(new Vector3(13.5f, 09f, 3f));
                 localPlayer.RpcChangeAudioIndex(2,0);
+                break;
+        }
+    }
+
+    public void OphiophobieLevelSelection()
+    {
+        int selectedIndex = ophiophobieButton.value;
+        localPlayer.CmdChangeSkybox(selectedIndex);
+
+        switch (selectedIndex)
+        {
+            case 0:
+                TeleportPatientToPosition(new Vector3(25f, 30f, 25f));
+                localPlayer.RpcChangeAudioIndex(0,2);
+                break;
+            case 1:
+                TeleportPatientToPosition(new Vector3(25f, 30f, 25f));
+                localPlayer.RpcChangeAudioIndex(1,2);
+                break;
+            case 2:
+                TeleportPatientToPosition(new Vector3(5f, 1f, 3f));
+                localPlayer.RpcChangeAudioIndex(2,2);
+                break;
+            case 3:
+                TeleportPatientToPosition(new Vector3(5f, 1f, 3f));
+                localPlayer.RpcChangeAudioIndex(0,2);
+                break;
+            case 4:
+                TeleportPatientToPosition(new Vector3(5f, 1f, 3f));
+                localPlayer.RpcChangeAudioIndex(0, 2);
+                break;
+            case 5:
+                TeleportPatientToPosition(new Vector3(13.5f, 09f, 3f));
+                localPlayer.RpcChangeAudioIndex(2,2);
                 break;
         }
     }
